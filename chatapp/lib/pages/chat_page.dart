d5e1199a58b0b3cd433bd4a5d14b968d1a695c80
nikunj63 <nikunjs6324@gmail.com>
2024-10-05 17:00:34 +0530

    eight'th

--- chatapp/lib/pages/chat_page.dart
+++ chatapp/lib/pages/chat_page.dart
@@ -1,3 +1,5 @@
+import 'package:chatapp/services/chat/chat_services.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class ChatPage extends StatefulWidget {
@@ -14,6 +16,20 @@ class ChatPage extends StatefulWidget {
 }
 
 class _ChatPageState extends State<ChatPage> {
+  final TextEditingController _messageControler = TextEditingController();
+  final ChatServices _chatServices = ChatServices();
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+
+  void sendMessage()async{
+    // only send message if somthing to send
+    if (_messageControler.text.isNotEmpty) {
+      await _chatServices.sendMessage(
+        widget.receiverUserID, _messageControler.text
+      );
+      // clear the text controller after sending mesaage
+      _messageControler.clear();
+    }
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
