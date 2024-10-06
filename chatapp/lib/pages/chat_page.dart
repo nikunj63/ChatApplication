d2a73dc39298e38aec6450b330e27c721b8dcd62
nikunj63 <nikunjs6324@gmail.com>
2024-10-06 18:28:47 +0530

    nineth

--- chatapp/lib/pages/chat_page.dart
+++ chatapp/lib/pages/chat_page.dart
@@ -1,4 +1,6 @@
+import 'package:chatapp/components/My_text_field.dart';
 import 'package:chatapp/services/chat/chat_services.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
@@ -34,8 +36,81 @@ class _ChatPageState extends State<ChatPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.receiverUserEmail),
+        title: Text(widget.receiverUserEmail), 
       ),
+      body:Column(
+        children: [
+          // message
+          Expanded(
+            child: _buildMessageList()
+          ),
+          // user input
+          _buildMessageInput(),
+        ],
+      ),
+    );
+  }
+// build message list
+Widget _buildMessageList(){
+  return StreamBuilder(
+    stream: _chatServices.getMesssages(
+      widget.receiverUserID, _firebaseAuth.currentUser!.uid,
+      ), 
+    builder: (context ,snapshot){
+      if(snapshot.hasError){
+        return Text('Error${snapshot.error}');
+      }
+      if (snapshot.connectionState == ConnectionState.waiting) {
+        return const Text('Loading');
+      }
+      return ListView(
+        children: snapshot.data!.docs
+        .map((document) => _buildMessageItem(document))
+        .toList(),
+      );
+    }
+    );
+}
+ 
+// build message item
+  Widget _buildMessageItem(DocumentSnapshot document){
+    Map<String ,dynamic> data = document.data() as Map<String ,dynamic>;
+    // align the messages to the right if the sender is the current user, otherwise to the left
+  var alignment = (data['senderId'] == _firebaseAuth.currentUser!.uid)
+  ? Alignment.centerRight
+  : Alignment.centerLeft;
+
+  return Container(
+    alignment: alignment,
+    child: Column(
+      children: [
+        Text( data['senderEmail']),
+        Text(data['message']),
+      ],
+    ),
+  );
+  }
+
+// build message input
+  Widget _buildMessageInput(){
+    return Row(
+      children: [
+        // text field
+        Expanded(
+          child: MyTextField(
+            Controller: _messageControler,
+             hintText: 'Enter message', 
+             obscureText: false,
+             )
+        ),
+        // send button
+        IconButton(
+          onPressed:sendMessage, 
+         icon: const Icon(Icons.arrow_upward,
+         size: 40,
+         )
+        )
+      ],
     );
   }
 }
